d6e131b35bd7df87bbcf5840123a744ce39e63af
Ivan Rychkov <rychkov.ivan@gmail.com>
2023-05-17 16:18:47 +0300

    fix dm_products.sql

--- dags/dds/sql/dm_products.sql
+++ dags/dds/sql/dm_products.sql
@@ -2,16 +2,16 @@ delete
 from dds.dm_products
 where true;
 alter sequence dds.dm_products_id_seq restart with 1;
-with products as (select id, json_array_elements(object_value::json -> 'menu') as p, update_ts
+with products as (select object_id as restaurant_id, json_array_elements(object_value::json -> 'menu') as p, update_ts
                   from stg.ordersystem_restaurants)
 insert
 into dds.dm_products (restaurant_id, product_id, product_name, product_price, active_from, active_to)
 select r.id,
-       p -> '_id',
+       p ->> '_id',
        p ->> 'name',
        (p ->> 'price')::numeric(14, 2),
        update_ts,
        timestamp '2099-12-31 00:00:00.000'
 from products p
-         join dds.dm_restaurants r on p.id = r.id
-;
+         join dds.dm_restaurants r using (restaurant_id)
+order by 1, 2
