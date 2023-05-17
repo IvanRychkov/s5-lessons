dab0c9b86d7e656ffc9f55326a8eb47c8405f53a
Ivan Rychkov <rychkov.ivan@gmail.com>
2023-05-17 13:12:28 +0300

    add dm_timestamps.sql

---
+++ dags/dds/sql/dm_timestamps.sql
@@ -0,0 +1,18 @@
+delete
+from dds.dm_timestamps
+where true;
+alter sequence dds.dm_timestamps_id_seq restart with 1;
+with jsoned as (select (object_value ::json #>> '{}')::json as j
+                from stg.ordersystem_orders),
+     dates as (select distinct (j ->> 'date')::timestamp as ts
+               from jsoned
+               where j ->> 'final_status' in ('CLOSED', 'CANCELLED'))
+insert
+into dds.dm_timestamps (ts, year, month, day, time, date)
+select ts,
+       extract(year from ts),
+       extract(month from ts),
+       extract(day from ts),
+       ts::time,
+       ts::date
+from dates;
